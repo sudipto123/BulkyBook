a797ce9bc15e7747cda6c61c9480e22cf2362778
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2023-03-20 12:25:12 +0530

    12. Hot Reload

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//Hot Reload was introduced in Dot Net 6
+//builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
