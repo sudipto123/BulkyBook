c58eb8932b4be341aa4ee93c323854d3728f2f5c
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2023-03-31 20:55:29 +0530

    286. Areas in Action

--- BulkyBookWeb/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -3,8 +3,9 @@ using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BulkyBookWeb.Controllers
+namespace BulkyBookWeb.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
@@ -41,43 +42,43 @@ namespace BulkyBookWeb.Controllers
             return View(obj);
         }
 
-		//GET
-		public IActionResult Edit(int? id)
-		{
-			if (id == null || id == 0)
-			{
-				return NotFound();
-			}
-			//var categoryFromDb = _db.Categories.Find(id);
-			var categoryFromDbFirst = _unitOfWork.Category.GetFirstOrDefault(u=>u.Id==id);
-			//var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            //var categoryFromDb = _db.Categories.Find(id);
+            var categoryFromDbFirst = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
 
-			if (categoryFromDbFirst == null)
-			{
-				return NotFound();
-			}
+            if (categoryFromDbFirst == null)
+            {
+                return NotFound();
+            }
 
-			return View(categoryFromDbFirst);
-		}
+            return View(categoryFromDbFirst);
+        }
 
-		//POST
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public IActionResult Edit(Category obj)
-		{
-			if (obj.Name == obj.DisplayOrder.ToString())
-			{
-				ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
-			}
-			if (ModelState.IsValid)
-			{
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
                 _unitOfWork.Category.Update(obj);
                 _unitOfWork.Save();
-				TempData["success"] = "Category updated successfully";
-				return RedirectToAction("Index");
-			}
-			return View(obj);
-		}
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
 
         //GET
         public IActionResult Delete(int? id)
@@ -88,7 +89,7 @@ namespace BulkyBookWeb.Controllers
             }
             //var categoryFromDb = _db.Categories.Find(id);
 
-            var categoryFromDbFirst = _unitOfWork.Category.GetFirstOrDefault(u=>u.Id==id);
+            var categoryFromDbFirst = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
 
             //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
 
@@ -113,8 +114,8 @@ namespace BulkyBookWeb.Controllers
 
             _unitOfWork.Category.Remove(obj);
             _unitOfWork.Save();
-			TempData["success"] = "Category Deleted successfully";
-			return RedirectToAction("Index");
+            TempData["success"] = "Category Deleted successfully";
+            return RedirectToAction("Index");
 
         }
     }
