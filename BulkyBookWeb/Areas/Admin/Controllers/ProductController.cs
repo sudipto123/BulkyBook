85b340b641749bb8818674b782d836bd8c69f999
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2023-04-02 00:00:55 +0530

    314. View Models

--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using BulkyBook.DataAccess.Data;
 using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
@@ -24,29 +25,28 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
         //GET
         public IActionResult Upsert(int? id)
         {
-            Product product = new();
-
-            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(
-            u => new SelectListItem
+            ProductVM productVM = new()
             {
-                Text = u.Name,
-                Value = u.Id.ToString()
-            });
-
-			IEnumerable<SelectListItem> CoverTypeList = _unitOfWork.CoverType.GetAll().Select(
-			u => new SelectListItem
-			{
-				Text = u.Name,
-				Value = u.Id.ToString()
-			});
+                Product = new(),
+                CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+				CoverTypeList = _unitOfWork.CoverType.GetAll().Select(i => new SelectListItem
+				{
+					Text = i.Name,
+					Value = i.Id.ToString()
+				})
+			};
 
 			if (id == null || id == 0)
             {
                 // Craete Product
-                ViewBag.CategoryList = CategoryList;
-                ViewData["CoverTypeList"] = CoverTypeList;
+                //ViewBag.CategoryList = CategoryList;
+                //ViewData["CoverTypeList"] = CoverTypeList;
 
-				return View(product);
+				return View(productVM);
             }
             else
             {
@@ -54,7 +54,7 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
 
             }
 
-            return View(product);
+            return View(productVM);
         }
 
         //POST
