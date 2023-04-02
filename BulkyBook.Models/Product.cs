3ac5d3f364eeb8bb92d6c1b67e88dcf93a914d79
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2023-04-02 12:53:39 +0530

    317. Create First Product

--- BulkyBook.Models/Product.cs
+++ BulkyBook.Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -34,15 +35,18 @@ namespace BulkyBook.Models
 		[Range(1, 1000)]
 		public double Price100 { get; set; }
 		[Display(Name = "Image URL")]
+		[ValidateNever]
 		public string ImageUrl { get; set; }
 		[Required]
 		[Display(Name = "Category")]
 		public int CategoryId { get; set; }
 		[ForeignKey("CategoryId")]
+		[ValidateNever]
         public Category Category { get; set; }
 		[Required]
 		[Display(Name = "Cover Type")]
 		public int CoverTypeId { get; set; }
+		[ValidateNever]
 		public CoverType CoverType { get; set; }
 	}
 }
--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -12,9 +12,11 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public ProductController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _hostEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
         }
         public IActionResult Index()
         {
@@ -60,13 +62,27 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
         //POST
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Upsert(ProductVM obj, IFormFile file)
+        public IActionResult Upsert(ProductVM obj, IFormFile? file)
         {
             if (ModelState.IsValid)
             {
-                //_unitOfWork.Product.Update(obj);
+                string wwwRootPath = _hostEnvironment.WebRootPath;
+
+                if(file !=  null)
+                {
+                    string fileName = Guid.NewGuid().ToString();
+                    var uploads = Path.Combine(wwwRootPath, @"images\products");
+                    var extension = Path.GetExtension(file.FileName);
+                    using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+                    {
+                        file.CopyTo(fileStreams);
+                    }
+                    obj.Product.ImageUrl = @"\images\products\" + fileName + extension;
+                }
+
+                _unitOfWork.Product.Add(obj.Product);
                 _unitOfWork.Save();
-                TempData["success"] = "Product updated successfully";
+                TempData["success"] = "Product created successfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
